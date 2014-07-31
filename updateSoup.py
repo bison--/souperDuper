b231897d75f459115242b7c5535e9bc3724881b1
Dodo <dodo@chaosdorf.de>
2014-07-31 19:19:37 +0200

    documentation

--- updateSoup.py
+++ updateSoup.py
@@ -1,10 +1,10 @@
-from readSoup import souperDuper
 import os
 
 folder = os.getcwd() + '/images'
 folder = os.listdir(folder)
 
 for filename in folder:
+	# filename: "username.knownUrls.txt"
 	username = filename.split('.')[0]
 	print username
 	os.system('python readSoup.py ' + username)
